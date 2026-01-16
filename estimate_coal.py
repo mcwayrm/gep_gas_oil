c657afd8a390f63790347719e2adfd6755449c0e
Ryan McWay <ryan.mcway@gmail.com>
2026-01-16 12:58:02 -0600

    Updated method to rely on previously estimated CWON estimates.

--- estimate_coal.py
+++ estimate_coal.py
@@ -1,7 +1,5 @@
 """
-This script combines the world bank measures of national GDP with the % of gdp related to coal. 
-Then I adjust for nature's contribution. 
-This script produces a panel of country-year values for GEP by coal as an ecosystem service. 
+This script takes the CWON estimates for natural capital rents as national GEP values for coal. 
 """
 
 # Dependencies
@@ -10,80 +8,13 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np  
 
-
-def clean_wb_data(df, col_name):
-    # Rename variables: 
-    old_names = ['Country Name', 'Country Code']
-    new_names = ['country', 'country_code']
-
-    # Rename the columns
-    rename_dict = dict(zip(old_names, new_names))
-    df.rename(columns=rename_dict, inplace=True)
-
-    # Drop non-country attributes
-    countries_to_drop = ['Africa Eastern and Southern', 'Africa Western and Central', 'Arab World',
-                        'Caribbean small states', 'East Asia & Pacific (excluding high income)',
-                        'Early-demographic dividend', 'East Asia & Pacific', 'Europe & Central Asia (excluding high income)',
-                        'Europe & Central Asia', 'European Union', 'Fragile and conflict affected situations',
-                        'Heavily indebted poor countries (HIPC)', 'IBRD only', 'IDA & IBRD total', 'IDA total',
-                        'IDA blend', 'IDA only', 'Not classified', 'Latin America & Caribbean (excluding high income)',
-                        'Latin America & Caribbean', 'Least developed countries: UN classification', 'Low income',
-                        'Lower middle income', 'Low & middle income', 'Late-demographic dividend', 'Middle income', 
-                        'Middle East & North Africa (excluding high income)', 'North America', 'OECD members', 
-                        'Other small states', 'Pre-demographic dividend', 'Pacific island small states',
-                        'Pacific island small states', 'Sub-Saharan Africa (excluding high income)', 'Sub-Saharan Africa', 'Small states',
-                        'East Asia & Pacific (IDA & IBRD countries)', 'Europe & Central Asia (IDA & IBRD countries)', 
-                        'Latin America & the Caribbean (IDA & IBRD countries)', 'Middle East & North Africa (IDA & IBRD countries)', 
-                        'South Asia (IDA & IBRD)', 'Sub-Saharan Africa (IDA & IBRD countries)', 'Upper middle income', 'World',]
-    df = df[~df['country'].isin(countries_to_drop)]
-
-    # Reshape to long format (year - country)
-    df = pd.melt(df, 
-                id_vars = ['country', 'country_code'], 
-                value_vars = [str(year) for year in range(1970, 2022)],  # Years 1970 to 2021
-                var_name = "year", 
-                value_name = col_name)
-    
-    # Assert as dataframe
-    df = pd.DataFrame(df)
-    # Return Dataframe
-    return(df)
-
-# Import: World Bank coal Rents: https://data.worldbank.org/indicator/NY.GDP.COAL.RT.ZS
-df_coal_rents = pd.read_excel("../data/world_bank/national_coal_edited.xls")
-# Import: World Bank GDP: https://data.worldbank.org/indicator/NY.GDP.MKTP.CD
-df_gdp = pd.read_excel("../data/world_bank/national_gdp_edited.xls")
-
-# Clean coal rents data 
-df_coal_rents = clean_wb_data(df_coal_rents, "coal")
-# Clean GDP data
-df_gdp = clean_wb_data(df_gdp, "gdp")
-
-# Merge GDP with coal rents data
-df_gep = pd.merge(left = df_coal_rents,
-            right = df_gdp,
-            how = 'inner')
-
-# Assign nature's contribution value through resource rent adjustment
-df_gep['year'] = df_gep['year'].astype(int)
-conditions = [
-    (df_gep['year'] <= 1995),
-    (df_gep['year'] >= 1996) & (df_gep['year'] <= 2000),
-    (df_gep['year'] >= 2001) & (df_gep['year'] <= 2005),
-    (df_gep['year'] >= 2006) & (df_gep['year'] <= 2010), 
-    (df_gep['year'] >= 2011) & (df_gep['year'] <= 2015),  
-    (df_gep['year'] >= 2016)  
-]
-# Define corresponding values
-values = [0.17, 0.29, 0.39, 0.37, 0.26, 0.20]
-# Create resource rent value
-df_gep['resource_rent'] = np.select(conditions, values, default=np.nan)
-
-# Estimate GEP value for gas 
-df_gep["gep_coal"] = df_gep["coal"] * df_gep["gdp"] * df_gep['resource_rent']
-
-# Only keep 2019 values 
-df_gep = df_gep[df_gep['year'] == 2019]
+# Import CWON Estimates 
+df_gep = pd.read_excel("../data/cwon-resource-rents/cwon-rents-coal.xlsx")
+# Keep only 2019 
+col_keep = ['countrycode', 'countryname', 'YR2019']
+df_gep = df_gep[col_keep]
+# Rename 2019 to gep_coal 
+df_gep.rename(columns= {'YR2019': 'gep_coal', 'countrycode': 'country_code', 'countryname': 'country'}, inplace=True)
 
 # Correctiong for ee_r250 country mapping
 file_path = "../data/ee_r250_correspondence.gpkg"
@@ -93,6 +24,6 @@ df_merged = pd.merge(gdf, df_gep,  how='left', left_on='adm0_a3', right_on='coun
 # Keep a subset of variables 
 df_merged = df_merged[['ee_r264_id', 'iso3_r250_id', 'iso3_r250_label', 'ee_r264_description', 'gep_coal']]
 
-# Save a csv file of country, year petrolium values
+# Save a csv file of country, year coal values
 df_gep = df_merged.sort_values(by = ['ee_r264_id'], ascending = [True])
 df_gep.to_csv("../data/gep-datasets/gep-coal.csv", index=False)
